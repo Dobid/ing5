d6d1c30cb9160bb730377a60044465efb20457d6
David <davidolivares68@gmail.com>
2020-10-14 21:44:34 +0200

    dépot campus

--- prog_parallele/seance2/TP2/1-ln2_approx_thread/ln2_approx_thread.c
+++ prog_parallele/seance2/TP2/1-ln2_approx_thread/ln2_approx_thread.c
@@ -48,3 +48,47 @@ void *thread_for (void *sum)
     }
     return (void *)0;
 }
+
+
+// void *thread_for (void *sum);
+
+// int main(void)
+// {
+//     pthread_t ids [THREAD_MAX];
+//     double sums [THREAD_MAX];
+//     int i;
+//     double sum = 0.0;
+//     #pragma omp parallel for
+//     for (i = 0; i < THREAD_MAX; i++)
+//     {
+//         sums [i] = (double) i;
+//         pthread_create (&ids[i], NULL, thread_for, (void *)&sums[i]);
+//     }
+//     for (i = THREAD_MAX - 1; i >= 0; i--)
+//     {
+//         pthread_join (ids[i], NULL);
+//         sum += sums [i];
+//     }
+//     printf ("sum: %.12f\n" , sum);
+//     return 0;
+// }
+
+// void *thread_for (void *sum)
+// {
+//     long long n, n_min, n_max;
+//     n_min = N_MAX / THREAD_MAX * *(double *)sum;
+//     n_max = N_MAX / THREAD_MAX * (*(double *)sum + 1) ;
+//     *(double *)sum = 0.0 ;
+//     for (n = n_max ; n > n_min ; n--)
+//     {
+//         if (n % 2 == 0)
+//         {
+//             *(double*)sum -= 1.0 / (double )n;
+//         }
+//         else
+//         {
+//             *(double *)sum += 1.0 / (double )n;
+//         }
+//     }
+//     return (void *)0;
+// }
