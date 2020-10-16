ce6cbc683ef50954765ddb6b5d826f225e516087
Palfi <emma.palfi@edu.ece.fr>
2020-10-16 22:34:32 +0200

    Exo 1 openmp ok

--- prog_parallele/seance3/TP3/1-sine_sync_openmp/sine_sync_openmp.c
+++ prog_parallele/seance3/TP3/1-sine_sync_openmp/sine_sync_openmp.c
@@ -8,31 +8,21 @@
 #define PI 3.14159265
 
 int sine_value = 0;
-static pthread_mutex_t mutex1;
-static pthread_mutex_t mutex2;
 
-void *sine_producer (void *thread_arg)
+int main (int argc, char **argv)
 {
+    omp_lock_t lock1;
+    omp_lock_t lock2;
+    omp_init_lock(&lock1);
+    omp_init_lock(&lock2);
+
     int phase = 0;
     int amplitude = 10000;
     float x = 0;
+   
+    omp_set_lock(&lock1);
+    omp_set_lock(&lock2);
 
-    #pragma omp parallel for private(phase)
-    for (phase = 0; phase < N_MAX; ++phase)
-    {
-        // La première fois le thread 1 calcule sine
-        x = 40 * 0.001 * phase;
-        sine_value = (int)(amplitude * sin(x));
-        int ret1 = pthread_mutex_unlock(&mutex1);
-        // Le thread 2 doit attendre que le thread 2 unlock le mutex 2
-        int ret2 = pthread_mutex_lock (&mutex2);
-    }
-    
-    return NULL;
-}
-
-void *sine_writer (void *thread_arg)
-{
     int nb_write = 0;
     FILE *file = NULL;
     file = fopen ("sine.txt" , "w");
@@ -40,52 +30,34 @@ void *sine_writer (void *thread_arg)
     {
         printf("ERROR while opening file\n");
     }
-    
-    #pragma omp parallel for private(nb_write)
-    for (nb_write = 0; nb_write < N_MAX; ++nb_write)
+
+    #pragma omp parallel sections
     {
-        // Le thread 2 doit attendre que le thread 1 unlock le mutex1
-        int ret1 = pthread_mutex_lock(&mutex1);
-        fprintf(file, "%d\t%d\n", nb_write, sine_value); 
-        int ret2 = pthread_mutex_unlock (&mutex2);
+        #pragma omp section
+        {
+            for (phase = 0; phase < N_MAX; ++phase)
+            {
+                x = 40 * 0.001 * phase; 
+                sine_value = (int)(amplitude * sin(x));
+                omp_unset_lock(&lock1);
+                omp_set_lock(&lock2);
+            }
+        }
+        
+        #pragma omp section
+        {
+            for (nb_write = 0; nb_write < N_MAX; ++nb_write)
+            {
+                #pragma omp critical
+                {
+                    omp_set_lock(&lock1);
+                    fprintf(file, "%d\t%d\n", nb_write, sine_value);
+                    omp_unset_lock(&lock2);
+                }   
+            }
+        }
     }
-    
+
     fclose(file);
-    
-    return NULL;
-}
-
-int main (int argc, char **argv)
-{
-    int n_threads;
-    int rc;
-    pthread_t *my_threads;
-    void *thread_return;
-
-    n_threads = 2;
-    my_threads = calloc(n_threads, sizeof(pthread_t));
-    
-    pthread_create(&my_threads[0], NULL, sine_writer, NULL);
-    pthread_create(&my_threads[1], NULL, sine_producer, NULL);
-
-    // On lock pour forcer le programme à commencer par le thread 1
-    int ret1 = pthread_mutex_lock(&mutex1);
-    int ret2 = pthread_mutex_lock(&mutex2);
-    
-    for (int i = 0; i < n_threads; i++)
-    {
-        rc = pthread_join(my_threads[i], &thread_return);
-        if (rc < 0)
-            printf("pthread_join ERROR !!! (%d)\n", rc);
-    }
-
-    // On unlock pour destroy sans problèmes
-    ret1 = pthread_mutex_unlock(&mutex1);
-    ret2 = pthread_mutex_unlock(&mutex2);
-
-    pthread_mutex_destroy(&mutex1);
-    pthread_mutex_destroy(&mutex2);
-    free (my_threads);
-
     return (0);
 }
